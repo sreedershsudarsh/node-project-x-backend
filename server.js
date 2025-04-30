6e3f71a14ea150cc6d6ddeb20b8458ff1d6a8abf
Sreedersh Sudarsh <sreedersh.sudarsh@cloudnautical.com>
2025-04-30 10:59:02 +0530

    newchange

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/hi', (req, res) => {
+  res.send('API is working!id snsidhjddjd nbn sreedeehshsh');
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
