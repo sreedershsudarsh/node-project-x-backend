6aac344d67e9cfe82b3b0ffb22384e78849f0da5
“sreedershsudarsh” <sreedershsudarsh9900@gmail.com>
2025-05-25 20:43:43 +0530

    cors add

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
-
+import cors from 'cors';
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/api', userRoutes); // Base path
 
