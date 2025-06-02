92c46beaeb4103cc9485235c3ca0622dba861035
“sreedershsudarsh” <sreedershsudarsh9900@gmail.com>
2025-06-02 22:31:21 +0530

    add string

--- models/User.js
+++ models/User.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
+  head: String,
+  price: String,
+  days:String,
+  lord:String,
+  time:String
 });
 
 export const User = mongoose.model('User', UserSchema);
